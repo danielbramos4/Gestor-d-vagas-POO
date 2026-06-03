51ab8e6a18ab9393a63dd37cd9b2f42a2e854252
Daniel Barbosa Ramos <danielramosdbr@hotmail.com>
2026-06-03 11:21:21 -0300

    Commit inicial: Estrutura base do projeto Spring Boot/Maven

---
+++ gestor-de-vagas-poo/src/main/java/br/com/danielbarbosa/gestordevagaspoo/GestorDeVagasPooApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielbarbosa.gestordevagaspoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeVagasPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeVagasPooApplication.class, args);
+	}
+
+}
